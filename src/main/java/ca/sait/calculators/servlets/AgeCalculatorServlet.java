1ec01b3d6c80b1c57eca461de2a5c6ce344a8bb3
Melvin Chew <melvincv15@gmail.com>
2022-01-15 15:42:25 -0700

    Test Remove Line
    Got rid of line 20

---
+++ src/main/java/ca/sait/calculators/servlets/AgeCalculatorServlet.java
@@ -0,0 +1,52 @@
+package ca.sait.calculators.servlets;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+/**
+ *
+ * @author Melvin Chew
+ */
+public class AgeCalculatorServlet extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String ageInput = request.getParameter("age");
+
+        int age = Integer.parseInt(ageInput);
+        
+        age++;
+
+        String message = String.format("You will be %d after your next birthday.", age);
+
+        request.setAttribute("message", message);
+
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
+    }
+
+
+}
---
+++ src/main/webapp/WEB-INF/agecalculator.jsp
@@ -0,0 +1,25 @@
+<%-- 
+    Document   : agecalculator
+    Created on : Jan 13, 2022, 11:08:04 PM
+    Author     : Melvin Chew
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Age Calculator</h1>
+        
+        <form action="age" method="post">
+            <label for="age">Enter your age:</label>
+            <input type="number" name="age" id="age">
+            <button type="submit">Submit</button>
+        </form>
+        
+        <p>${message}</p>
+    </body>
+</html>
